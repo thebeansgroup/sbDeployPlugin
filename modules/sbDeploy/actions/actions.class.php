0dbd44309ead2b0b0be9f78b727a666c2ded8174
Daniele <daniele@thebeansgroup.com>
2010-09-16 10:23:25 +0000

    fixed an incompatibility issue with Sf 1.4.6 on sbDeployPlugin

--- modules/sbDeploy/actions/actions.class.php
+++ modules/sbDeploy/actions/actions.class.php
@@ -22,7 +22,10 @@ class sbDeployActions extends sfActions
     }
 
     $this->projectName = TaskUtils::getProjectName();
-    $this->setLayout('sbDeployLayout');
+
+    // $this->setLayout('sbDeployLayout');
+    // the previous line isn't working on Symfony 1.4.6
+    $this->setLayout(false);
 
     $this->setupProductionFormActions();
     $this->setupStagingFormActions();
