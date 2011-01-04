d55a8360fe17e7d06074c7654458924e9daee6e5
Vincent Siebert <vincent@siebert.im>
2011-01-04 16:43:08 +0000

    adding new action when building staging

--- modules/sbDeploy/actions/actions.class.php
+++ modules/sbDeploy/actions/actions.class.php
@@ -349,7 +349,7 @@ class sbDeployActions extends sfActions
             'strpos' => array(
                 array(
                     'string' => 'succesfully minified',
-                    'test' => '===',
+                    'test' => '!==',
                     'value' => false
                 )
             )
@@ -365,7 +365,7 @@ class sbDeployActions extends sfActions
             'strpos' => array(
                 array(
                     'string' => '0 error(s)',
-                    'test' => '===',
+                    'test' => '!==',
                     'value' => false
                 )
             )
